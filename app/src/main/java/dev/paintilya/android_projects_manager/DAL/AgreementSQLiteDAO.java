dfdb1c9f67069a7575c432ac03d1960a61e0db80
Wolpher <wolpherqc@gmail.com>
2023-03-10 08:25:12 -0500

    HEHEHEHeh

--- app/src/main/java/dev/paintilya/android_projects_manager/DAL/AgreementSQLiteDAO.java
+++ app/src/main/java/dev/paintilya/android_projects_manager/DAL/AgreementSQLiteDAO.java
@@ -1,27 +1,82 @@
 package dev.paintilya.android_projects_manager.DAL;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import dev.paintilya.android_projects_manager.DL.SQLiteHelper;
 import dev.paintilya.android_projects_manager.Model.Agreement;
+import dev.paintilya.android_projects_manager.Model.PaymentMethod;
 
 public class AgreementSQLiteDAO implements IAgreementDAO{
-    @Override
-    public int addAgreementByProjectId(int projectId, Agreement newAgreement) {
-        return 0;
+
+    SQLiteHelper helper;
+
+    public AgreementSQLiteDAO(Context context) {
+        this.helper = new SQLiteHelper(context);
     }
 
     @Override
-    public List<Agreement> getAllAgreementsByProjectId(int projectId) {
+    public int addAgreementByProjectId(int projectId, Agreement newAgreement) {
+        SQLiteDatabase db = this.helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("id", newAgreement.getId());
+        values.put("projectId", newAgreement.getProjectId());
+        values.put("content", newAgreement.getContent());
+        values.put("subject", newAgreement.getSubject());
+        db.insert("Agreement", "id, projectId, content, subject", values);
+        return 0; // Success
+    }
+
+    @Override
+    public List<Agreement> getAllAgreementsByProjectId(int ProjectId) {
+        SQLiteDatabase db = this.helper.getReadableDatabase();
+        String request = "SELECT * FROM Agreement WHERE projectId == ProjectId";
+        Cursor cursor = db.rawQuery(request, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            List<Agreement> agreements = new ArrayList<>();
+            while (!cursor.isAfterLast()) {
+                Agreement agreement = new Agreement();
+                agreement.setId(cursor.getInt(0));
+                agreement.setProjectId(cursor.getInt(1));
+                agreement.setContent(cursor.getString(2));
+                agreement.setSubject(cursor.getString(3));
+                agreements.add(agreement);
+                cursor.moveToNext();
+            }
+            db.close();
+            return agreements;
+        }
+
         return null;
     }
 
     @Override
     public int updateAgreementById(int id, String newSubject, String newContent) {
+        SQLiteDatabase db = this.helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        Agreement agreement = new Agreement();
+        agreement.setSubject(newSubject);
+        agreement.setContent(newContent);
+        values.put("subject", agreement.getSubject());
+        values.put("content", agreement.getContent());
+
+        db.update("Agreement",  values, "id = ?", new String[]{ id+"" });
         return 0;
     }
 
     @Override
     public int deleteAgreementById(int id) {
-        return 0;
+        Agreement agreement = new Agreement();
+        if(agreement.getId() != id) {
+            SQLiteDatabase db = this.helper.getWritableDatabase();
+            db.delete("Agreement", "id = ?", new String[]{id + ""});
+        }
+        return 0; //success
     }
 }
--- app/src/main/java/dev/paintilya/android_projects_manager/DAL/BankAccountSQLiteDAO.java
+++ app/src/main/java/dev/paintilya/android_projects_manager/DAL/BankAccountSQLiteDAO.java
@@ -1,17 +1,57 @@
 package dev.paintilya.android_projects_manager.DAL;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import dev.paintilya.android_projects_manager.DL.SQLiteHelper;
+import dev.paintilya.android_projects_manager.Model.Agreement;
 import dev.paintilya.android_projects_manager.Model.BankAccount;
+import dev.paintilya.android_projects_manager.Model.PaymentMethod;
 
 public class BankAccountSQLiteDAO implements IBankAccountDAO{
+
+    SQLiteHelper helper;
+
+    public BankAccountSQLiteDAO(Context context) {
+        this.helper = new SQLiteHelper(context);
+    }
     @Override
     public List<BankAccount> getAllAccounts() {
+        SQLiteDatabase db = this.helper.getReadableDatabase();
+        String request = "SELECT * FROM BankAccount";
+        Cursor cursor = db.rawQuery(request, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            List<BankAccount> bankAccounts = new ArrayList<>();
+            while (!cursor.isAfterLast()) {
+                BankAccount bankAccount = new BankAccount();
+                bankAccount.setId(cursor.getInt(0));
+                bankAccount.setBalance(cursor.getDouble(1));
+                bankAccount.setName(cursor.getString(2));
+                bankAccounts.add(bankAccount);
+                cursor.moveToNext();
+            }
+            db.close();
+            return bankAccounts;
+        }
+
         return null;
     }
 
     @Override
     public int updateBankAccountBalanceById(int id, double newBalance) {
-        return 0;
+        SQLiteDatabase db = this.helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setBalance(newBalance);
+        values.put("subject", bankAccount.getBalance());
+
+        db.update("BankAccount",  values, "id = ?", new String[]{ id+"" });
+        return 0; //success
     }
 }
