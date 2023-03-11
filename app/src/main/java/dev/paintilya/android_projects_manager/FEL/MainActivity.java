f40620b7bbf401e33b62b5445754567f1e94e9b2
Mathis <paintilya@gmail.com>
2023-03-11 18:41:19 -0500

    MainActivity CAROTTÉ à 1$

--- app/src/main/java/dev/paintilya/android_projects_manager/FEL/MainActivity.java
+++ app/src/main/java/dev/paintilya/android_projects_manager/FEL/MainActivity.java
@@ -2,7 +2,9 @@ package dev.paintilya.android_projects_manager.FEL;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import dev.paintilya.android_projects_manager.DAL.BankAccountSQLiteDAO;
 import dev.paintilya.android_projects_manager.R;
@@ -13,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBankAccountsBtnClick(View view) {
+        Intent intent = new Intent(this, BankAccountsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onProjectsBtnClick(View view) {
+        Intent intent = new Intent(this, ProjectsActivity.class);
+        startActivity(intent);
     }
 }
